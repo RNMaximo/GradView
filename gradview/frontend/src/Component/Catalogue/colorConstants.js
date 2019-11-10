914abeb2add8cd0459c25bd9126b3bdb152b6359
Renato Noronha Maximo <ra176675@students.ic.unicamp.br>
2019-11-10 02:59:43 -0300

    visual: Torna as cores da visualização Teoria/Prática mais claras

--- gradview/frontend/src/Component/Catalogue/colorConstants.js
+++ gradview/frontend/src/Component/Catalogue/colorConstants.js
@@ -1,6 +1,6 @@
-export const TColor = "rgb(20,71,123)";
-export const PColor = "rgb(101,4,95)";
-export const LColor = "rgb(170,7,107)";
-export const OColor = "rgb(61,153,112)";
-export const DColor = "rgb(57,204,204)";
-export const RestColor = "rgb(35,35,35)";
+export const TColor = "rgb(40,91,143)";
+export const PColor = "rgb(130,20,130)";
+export const LColor = "rgb(200,30,130)";
+export const OColor = "rgb(60,150,110)";
+export const DColor = "rgb(50,200,200)";
+export const RestColor = "rgb(50,50,50)";
